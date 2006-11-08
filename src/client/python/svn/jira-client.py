af33bfe01d327abd9c74b0ac507e46c1b7262c13
Istvan Vamosi <istvan.vamosi@midori.hu>
2006-11-08 09:45:26 +0000

    SVN python client has been implemented.

--- src/client/python/svn/jira-client.py
+++ src/client/python/svn/jira-client.py
@@ -1,1 +1,56 @@
-# TODO complete this based on the Perl client
+#!/usr/bin/python
+
+# JIRA commit acceptance python client for SVN
+# Author: istvan.vamosi@midori.hu
+# $Id$
+
+import os
+import sys
+import urlparse
+import xmlrpclib
+
+# configure JIRA access
+jiraBaseURL = 'http://127.0.0.1:8080'
+jiraLogin = 'root'
+jiraPassword = 'root'
+
+# configure svnlook path
+svnlookPath = 'C:\\Progra~1\\svn-win32-1.4.0\\bin\\svnlook.exe'
+
+# get committer
+try:
+	f = os.popen(svnlookPath + ' author ' + sys.argv[1] + ' --transaction ' + sys.argv[2])
+	committer = f.read()
+	if f.close():
+		raise 1
+except:
+	print 'Unable to get committer with svnlook.'
+	sys.exit(1)
+
+# get commit message
+try:
+	f = os.popen(svnlookPath + ' log ' + sys.argv[1] + ' --transaction ' + sys.argv[2])
+	commitMessage = f.read()
+	if f.close():
+		raise 1
+except:
+	print 'Unable to get commit message with svnlook.'
+	sys.exit(1)
+
+# print arguments
+print >> sys.stderr, 'Committer: ' + committer
+print >> sys.stderr, 'Commit message: "' + commitMessage + '"'
+
+# invoke JIRA web service
+try:
+	s = xmlrpclib.ServerProxy(urlparse.urljoin(jiraBaseURL, '/rpc/xmlrpc'))
+	acceptance, comment = s.commitacc.acceptCommit(jiraLogin, jiraPassword, committer, commitMessage).split('|');
+except:
+	acceptance, comment = ['false', 'Unable to connect to the JIRA server at "' + jiraBaseURL + '".']
+
+if acceptance == 'true':
+	print >> sys.stderr, 'Commit accepted.'
+	sys.exit(0)
+else:
+	print >> sys.stderr, 'Commit rejected: ' + comment
+	sys.exit(1)
