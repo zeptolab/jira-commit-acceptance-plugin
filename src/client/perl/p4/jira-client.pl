12dfc58f361506d269924bccea6e0cb1d3310639
Ferenc Kiss <ferenc.kiss@midori.hu>
2007-10-20 13:50:55 +0000

    Initial working version of P4 perl client

--- src/client/perl/p4/jira-client.pl
+++ src/client/perl/p4/jira-client.pl
@@ -14,26 +14,31 @@ use XMLRPC::Lite;
 # If you specify multiple keys, the commit will be accepted if at least one project listed accepts it.
 # Or you can specify 'my $projectKey = "*";' to force using the global commit acceptance settings if you don't
 # want to specify any exact project key.)
-my $jiraBaseURL = "http://127.0.0.1:8080";
-my $jiraLogin = "root";
-my $jiraPassword = "root";
+my $jiraBaseURL = "http://127.0.0.1:8083";
+my $jiraLogin = "admin";
+my $jiraPassword = "admin";
 my $projectKey = "TST";
+
+# configure p4 path
+my $p4Path = "p4";
 
 # get committer passed as arg[0]
 my $committer = $ARGV[0];
 
-# slurp log message from log message file passed as arg[1]
+# get change description with change passed as arg[1]
 my $holdTerminator = $/;
 undef $/;
 
-open IN, "< $ARGV[1]" or die "Unable to open $ARGV[1].\n";
+open IN, '-|', "$p4Path -s describe $ARGV[1]" or die "Unable to get change description with p4.\n";
 my $buffer = <IN>;
 close IN;
 
 $/ = $holdTerminator;
 my @lines = split /$holdTerminator/, $buffer;
+@lines = map(substr($_, 1), grep(/^\t/, map(substr($_, 6) , grep(/^text:/, @lines)))); # TODO review and comment
 $buffer = "init";
 $buffer = join $holdTerminator, @lines;
+chomp($buffer);
 
 my $commitMessage = $buffer;
 
