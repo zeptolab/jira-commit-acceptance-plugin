cde0ccc90fd0c41c91c9bb8f5ff063285a2a0dcd
David Chui <dchui@atlassian.com>
2009-02-23 01:53:15 +0000

    ResourceBundles are added to i18NBean now, instead of their locations.

--- src/main/java/com/atlassian/jira/ext/commitacceptance/server/evaluator/predicate/AbstractPredicate.java
+++ src/main/java/com/atlassian/jira/ext/commitacceptance/server/evaluator/predicate/AbstractPredicate.java
@@ -1,8 +1,17 @@
 package com.atlassian.jira.ext.commitacceptance.server.evaluator.predicate;
 
 import com.atlassian.jira.ComponentManager;
+import com.atlassian.jira.user.preferences.PreferenceKeys;
+import com.atlassian.jira.user.preferences.JiraUserPreferences;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.web.bean.I18nBean;
+import com.atlassian.jira.web.util.JiraLocaleUtils;
+import com.opensymphony.user.User;
+
+import java.util.ResourceBundle;
+import java.util.Locale;
+
+import org.apache.commons.lang.StringUtils;
 
 public abstract class AbstractPredicate implements JiraPredicate {
 	
@@ -14,9 +23,30 @@ public abstract class AbstractPredicate implements JiraPredicate {
 
 	protected I18nBean getI18nBean() {
 		if (null == i18nBean) {
-			i18nBean = new I18nBean(getJiraAuthenticationContext().getUser(), "templates/commitacceptance-plugin");
+			i18nBean = new I18nBean(getJiraAuthenticationContext().getUser());
+            i18nBean.addBundle(
+                    ResourceBundle.getBundle(
+                            "templates.commitacceptance-plugin",
+                            getLocaleFromUser(getJiraAuthenticationContext().getUser())
+                    )
+            );
 		}
 		
 		return i18nBean;
 	}
+
+    private Locale getLocaleFromUser(final User user)
+    {
+        if (user != null)
+        {
+            final JiraUserPreferences userPrefs = new JiraUserPreferences(user, false);
+            final String localeStr = userPrefs.getString(PreferenceKeys.USER_LOCALE);
+
+            if (!StringUtils.isBlank(localeStr))
+            {
+                return JiraLocaleUtils.parseLocale(localeStr);
+            }
+        }
+        return ComponentManager.getInstance().getApplicationProperties().getDefaultLocale();
+    }
 }
